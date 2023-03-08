dccfccd417500f245de98582e3587d519b6421b1
uzair1761 <ozairkhanan@gmail.com>
2023-03-08 13:53:00 +0500

    create a birthday app

---
+++ src/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+    {
+      people.map((data)=>{
+        const {id,name,age,image} = data
+        return(
+          <article key={id} className='person'>
+            <img src={image} alt={name}></img>
+            <div>
+           <h4>{name}</h4>
+             <p>{age} years</p>
+
+        </div>
+        </article>
+        )
+      })
+    }
+    </>
+  );
+};
+
+export default List;
